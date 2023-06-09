3cd77be94de42f23d75148159bded7ff3ce999ce
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-06-09 15:51:56 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
 	# Gives pip some metadata about the package. Also displays on the PyPi page.
 	classifiers=[
 		"Programming Language :: Python :: 3.8",
-		"License :: OSI Approved :: Apache Software License 2.0",
+		"License :: OSI Approved :: Apache Software License",
 		"Operating System :: OS Independent",
 	],
 	# The version of Python that is required.
